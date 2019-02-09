c2ad01a7f24bcbe6a294e76876c77e159ad1c645
Tara Sherman <taradelin@gmail.com>
2019-02-09 14:09:47 -0500

    object-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -142,8 +142,8 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-// const largerPopulation = [];
-// console.log(largerPopulation);
+const lowerPopulation = zooAnimals.filter(animal => animal.population < 5);
+console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
 
